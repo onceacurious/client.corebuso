7745a9383e133ba1c3613ac7bce4bae5acc399b4
onceacurious <francisdeoaurelio.fda@gmail.com>
2022-12-21 07:52:55 +0800

    Init repo

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import "./app.scss";
+import "../src/assets/scss/main.scss";
+
+const App = () => {
+    return (
+        <div className="container">
+            <h1>App</h1>
+        </div>
+    );
+};
+
+export default App;
