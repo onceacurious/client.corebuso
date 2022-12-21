348ae29d8c038dafddc838f37e5c2e4d435886d5
onceacurious <francisdeoaurelio.fda@gmail.com>
2022-12-22 00:28:46 +0800

    nc

--- src/components/searchbar/Searchbar.jsx
+++ src/components/searchbar/Searchbar.jsx
@@ -1,22 +1,48 @@
-import React from 'react'
-import { ImSearch } from 'react-icons/im'
+import React, { useState, useEffect } from "react";
+import { ImSearch } from "react-icons/im";
 
-import './searchbar.scss'
+import "./searchbar.scss";
 const Searchbar = () => {
+    const [isFocus, setIsFocus] = useState(false);
 
     const handleSubmit = (e) => {
         e.preventDefault();
     };
+
     return (
-        <div className='container p-block-2'>
-            <form className='searchbar-form bg-primary-75' onSubmit={handleSubmit}>
-                <input className='searchbar-input' type="text" placeholder='Search...' />
-                <button className='searchbar-btn btn' type="submit">
+        <div className="container p-block-2">
+            <form
+                className={
+                    isFocus
+                        ? "searchbar-form bg-primary-75 form_input-focus"
+                        : "searchbar-form bg-primary-75"
+                }
+                onSubmit={handleSubmit}
+            >
+                <input
+                    className={
+                        isFocus
+                            ? "searchbar-input input-focus"
+                            : "searchbar-input"
+                    }
+                    type="text"
+                    placeholder="Search..."
+                    onFocus={() => setIsFocus(true)}
+                    onBlur={() => setIsFocus(false)}
+                />
+                <button
+                    className={
+                        isFocus
+                            ? "searchbar-btn btn btn_input-focus"
+                            : "searchbar-btn btn"
+                    }
+                    type="submit"
+                >
                     <ImSearch />
                 </button>
             </form>
         </div>
-    )
-}
+    );
+};
 
-export default Searchbar
+export default Searchbar;
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,30 +1,39 @@
-import React, { useEffect, useContext, useState } from 'react'
-import { Navbar, Searchbar } from '../../components'
+import React, { useEffect, useContext, useState } from "react";
+import { Navbar, Searchbar } from "../../components";
 
-import MainContext from '../../context/MainContext'
-import "./home.scss"
+import MainContext from "../../context/MainContext";
+import "./home.scss";
 const Home = () => {
     const { dark } = useContext(MainContext);
 
     return (
-        <div id="home" className={dark ? 'gradient-bg-dark' : 'gradient-bg-light'}>
+        <div
+            id="home"
+            className={dark ? "gradient-bg-dark" : "gradient-bg-light"}
+        >
             <Navbar />
             <Searchbar />
-            <div className='container home-body display-flex flex-coloumn jc-center ai-center'>
-                <p className='heading-4 gradient-text'>Let's make your dream come true</p>
-                <p className='heading-2 gradient-text'>CORE BUSINESS SOLUTION</p>
-                <p className='gradient-text'>Website Developemt and Accounting Services</p>
-                <div className='cta-container dispaly-flex m-block-4'>
-                    <button className='cta-btn btn-outline-primary btn fs-regular fw-bold btn-bordered'>
+            <div className="container home-body display-flex flex-column jc-center ai-center">
+                <p className="heading-4 gradient-text">
+                    Let's make your dream come true
+                </p>
+                <p className="heading-2 gradient-text">
+                    CORE BUSINESS SOLUTION
+                </p>
+                <p className="gradient-text">
+                    Website Development and Accounting Services
+                </p>
+                <div className="cta-container display-flex m-block-4 jc-center">
+                    <button className="cta-btn btn-outline-primary btn fs-regular fw-bol">
                         Get Started
                     </button>
-                    <button className='cta-btn btn-secondary btn fs-regular fw-bold'>
+                    <button className="cta-btn btn-secondary btn fs-regular fw-bold">
                         Request a Demo
                     </button>
                 </div>
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default Home
+export default Home;
